c85f673d581544fdedc2aa885c68ca40ef10f0c1
joelbowen <jmichaelb722@gmail.com>
2016-09-09 20:40:06 -0400

    Webpack, SASS, import CSS deps

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+
+module.exports = {
+  context: __dirname,
+  entry: './client/App.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'assets/bundle.js'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.json']
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: false
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        include: path.join(__dirname, '/client'),
+        loaders: [
+          'style',
+          'css',
+          'autoprefixer?browsers=last 3 versions',
+          'sass?outputStyle=expanded'
+        ]
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2)(.v=[0-9].[0-9].[0-9])?/,
+        loader: `file?name=/assets/fonts/[name].[ext]`
+      }
+    ]
+  }
+}
